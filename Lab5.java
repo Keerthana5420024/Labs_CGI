c30e4ae091a4b503e8bbb8d6e869c37061e40cb9
Keerthana5420024 <keerthana5420024@gmail.com>
2025-08-25 14:53:52 +0530

    commit

--- Lab5.java
+++ Lab5.java
@@ -4,16 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.time.Duration;
 
 public class Lab5 {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.get("https://tutorialsninja.com/demo/index.php?");
         driver.findElement(By.linkText("My Account")).click();
@@ -24,18 +20,18 @@ public class Lab5 {
 
         // First Continue click without filling fields
         driver.findElement(By.xpath("//*[@id='content']/form/div/div/input[2]")).click();
-        WebElement msg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='account-register']/div[1]")));
+        WebElement msg = driver.findElement(By.xpath("//*[@id='account-register']/div[1]"));
         System.out.println("The message is: " + msg.getText());
 
         // Fill first name
         WebElement search = driver.findElement(By.id("input-firstname"));
         search.sendKeys("Automation Testing Tools like Selenium and more");
         driver.findElement(By.xpath("//*[@id='content']/form/div/div/input[2]")).click();
-        WebElement msg1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"account\"]/div[2]/div/div")));
+        WebElement msg1 = driver.findElement(By.xpath("//*[@id=\"account\"]/div[2]/div/div"));
         System.out.println("The message is: " + msg1.getText());
 
         // Re-locate first name after page update
-        WebElement search1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-firstname")));
+        WebElement search1 = driver.findElement(By.id("input-firstname"));
         search1.clear();
         search1.sendKeys("Keerthana");
 
@@ -43,11 +39,11 @@ public class Lab5 {
         WebElement search2 = driver.findElement(By.id("input-lastname"));
         search2.sendKeys("Automation Testing Tools like Selenium and more");
         driver.findElement(By.xpath("//*[@id='content']/form/div/div/input[2]")).click();
-        WebElement msg2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"account\"]/div[3]/div/div")));
+        WebElement msg2 = driver.findElement(By.xpath("//*[@id=\"account\"]/div[3]/div/div"));
         System.out.println("The message is: " + msg2.getText());
 
         // Re-locate last name after page update
-        WebElement search3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-lastname")));
+        WebElement search3 = driver.findElement(By.id("input-lastname"));
         search3.clear();
         search3.sendKeys("Shetty");
 
@@ -67,23 +63,24 @@ public class Lab5 {
         WebElement confirm = driver.findElement(By.id("input-confirm"));
         confirm.sendKeys("abcd");
 
-       
+        // Select newsletter option
         driver.findElement(By.xpath("//*[@id=\"content\"]/form/fieldset[3]/div/div/label[1]/input")).click();
 
-       
+        // Agree to Privacy Policy
         driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/div/input[1]")).click();
 
-       
+        // Click Continue
         driver.findElement(By.xpath("//*[@id='content']/form/div/div/input[2]")).click();
 
-       
-        WebElement msg3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/h1")));
+        WebElement msg3 = driver.findElement(By.xpath("//*[@id=\"content\"]/h1"));
         System.out.println("The message is: " + msg3.getText());
-        
+
+        // Click Continue after successful registration
         driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/a")).click();
-        
+
+        // Logout
         driver.findElement(By.xpath("//*[@id=\"content\"]/ul[2]/li[1]/a")).click();
 
-
+        driver.quit();
     }
 }
